c09c26de97edd824fb6172ceb44cfdf67d2ef2c3
Nayar Joolfoo <njoolfoo@gmail.com>
2013-10-18 19:31:55 +0400

    initial adding of files

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "Mera OpenERP Module",
+    "version" : "1.0",
+    "author" : "Nayar",
+    "website" : "https://github.com/Nayar/mera-openerp-module",
+    "category" : "Generic Modules",
+    "depends" : ["base"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "installable": True,
+    "active": True
+} 
