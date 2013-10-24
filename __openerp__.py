8355ab9a22af2abfc2ebe61a52fc6b675ebb1f9d
Nayar Joolfoo <nayar@joolfoo.com>
2013-10-24 14:50:00 +0400

    MAde priority 1

--- __openerp__.py
+++ __openerp__.py
@@ -2,6 +2,7 @@
     "name" : "Mera OpenERP Module",
     "version" : "1.0",
     "author" : "Nayar",
+    "sequence" : 1,
     "website" : "https://github.com/Nayar/mera-openerp-module",
     "category" : "Generic Modules",
     "depends" : ["base"],
